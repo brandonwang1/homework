e18573e153f51422948316f4827e17b81c3f9438
Brandon Wang <13385547+brandonwang1@users.noreply.github.com>
2023-01-19 17:13:22 -0500

    hw1

--- assignments/week1/model.py
+++ assignments/week1/model.py
@@ -2,18 +2,35 @@ import numpy as np
 
 
 class LinearRegression:
-
+    """
+    Class for a linear regression model fitted using least squares.
+    """
     w: np.ndarray
     b: float
 
     def __init__(self):
-        raise NotImplementedError()
+        self.w = np.array([0, 0])
+        self.b = 0
 
-    def fit(self, X, y):
-        raise NotImplementedError()
+    def fit(self, X: np.ndarray, y: np.ndarray) -> None:
+        """
+        Fit the model to the given input and output data.
+        X: (n, d) array of input data
+        y: (n,) array of output data
+        Ref https://numpy.org/doc/stable/reference/generated/numpy.linalg.lstsq.html
+        """
+        A = np.hstack((X, np.ones((X.shape[0], 1))))
+        m = np.linalg.lstsq(A, y, rcond=None)[0]
+        self.w = m[:-1]
+        self.b = m[-1]
 
-    def predict(self, X):
-        raise NotImplementedError()
+    def predict(self, X: np.ndarray) -> np.ndarray:
+        """
+        Predict the output for the given input.
+        X: (n, d) array of input data
+        Returns: (n,) array of output data
+        """
+        return X @ self.w + self.b
 
 
 class GradientDescentLinearRegression(LinearRegression):
@@ -24,7 +41,26 @@ class GradientDescentLinearRegression(LinearRegression):
     def fit(
         self, X: np.ndarray, y: np.ndarray, lr: float = 0.01, epochs: int = 1000
     ) -> None:
-        raise NotImplementedError()
+        """
+        Fit the model to the given input and output data.
+
+        Arguments:
+            X (np.ndarray): The input data.
+            y (np.ndarray): The output data.
+            lr (float): The learning rate.
+            epochs (int): The number of epochs to train for.
+
+        Returns:
+            None
+        """
+        A = np.hstack((X, np.ones((X.shape[0], 1))))
+        self.w = np.zeros(X.shape[1])
+        self.b = 0
+        for _ in range(epochs):
+            y_hat = A @ np.hstack((self.w, self.b))
+            grad = A.T @ (y_hat - y)
+            self.w -= lr * grad[:-1]
+            self.b -= lr * grad[-1]
 
     def predict(self, X: np.ndarray) -> np.ndarray:
         """
@@ -37,4 +73,4 @@ class GradientDescentLinearRegression(LinearRegression):
             np.ndarray: The predicted output.
 
         """
-        raise NotImplementedError()
+        return X @ self.w + self.b
