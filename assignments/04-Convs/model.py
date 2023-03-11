ea8bcf8c3c96fcabb767b7f2fef36985b884dff8
Brandon Wang <13385547+brandonwang1@users.noreply.github.com>
2023-03-10 19:13:43 -0600

    init

--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -1,9 +1,33 @@
+import torch.nn as nn
 import torch
 
-
-class Model(torch.nn.Module):
+class Model(nn.Module):
     def __init__(self, num_channels: int, num_classes: int) -> None:
-        ...
+        super(Model, self).__init__()
+        
+        self.conv1 = nn.Conv2d(num_channels, 32, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm2d(32)
+        self.relu1 = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv2d(32, 32, kernel_size=3, padding=1)
+        self.bn2 = nn.BatchNorm2d(32)
+        self.relu2 = nn.ReLU(inplace=True)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.fc1 = nn.Linear(64 * 16 * 8, 512)
+        self.bn3 = nn.BatchNorm1d(512)
+        self.relu3 = nn.ReLU(inplace=True)
+        self.fc2 = nn.Linear(512, num_classes)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        ...
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu1(x)
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.relu2(x)
+        x = self.pool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc1(x)
+        x = self.bn3(x)
+        x = self.relu3(x)
+        x = self.fc2(x)
+        return x
