2660d6d77e7794a16ac8f01b6478b3c95a169e1f
Brandon Wang <13385547+brandonwang1@users.noreply.github.com>
2023-03-10 19:51:55 -0600

    fmt

--- assignments/04-Convs/config.py
+++ assignments/04-Convs/config.py
@@ -13,4 +13,6 @@ class CONFIG:
         [nn.Module], torch.optim.Optimizer
     ] = lambda model: torch.optim.Adam(model.parameters(), lr=1e-3)
 
-    transforms = Compose([ToTensor(), Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
+    transforms = Compose(
+        [ToTensor(), Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])]
+    )
